5417cf87712b613ca7f8ae1ba0b662cc90f3c8c9
ludekcz <dvorakludekcz@seznam.cz>
2023-01-20 19:33:46 +0100

    Vytvoření Quote Controlleru, kde bude výstup na připojení mého API jako http://localhost:8072/api

--- complete/src/main/java/com/example/consumingrest/ConsumingRestApplication.java
+++ complete/src/main/java/com/example/consumingrest/ConsumingRestApplication.java
@@ -7,17 +7,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-
+@RestController
 @SpringBootApplication
 public class ConsumingRestApplication {
 
 
 	private static final Logger log = LoggerFactory.getLogger(ConsumingRestApplication.class);
-
+	@GetMapping("/api")
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumingRestApplication.class, args);
+
+		String url = "https://catfact.ninja/fact?max_length=100"; //https://catfact.ninja/
 	}
 
 	@Bean
@@ -29,7 +33,7 @@ public class ConsumingRestApplication {
 	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
 		return args -> {
 			Quote quote = restTemplate.getForObject(
-					"http://localhost:8072/api", Quote.class);
+					"http://localhost:8072", Quote.class);
 			log.info(quote.toString()); //Dobrý den
 		};
 	}
--- complete/src/main/java/com/example/consumingrest/Quote.java
+++ complete/src/main/java/com/example/consumingrest/Quote.java
@@ -1,8 +1,28 @@
 package com.example.consumingrest;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record Quote(String type, Value value) {
+    public Quote {
+    }
 
+    @Override
+    public String type() {
+        return type;
+    }
+
+    @Override
+    public Value value() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return "Quote{" +
+                "type='" + type + '\'' +
+                ", value=" + value +
+                '}';
+    }
 }
